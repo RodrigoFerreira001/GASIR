b55a398339142df1d470b71d8dff330771b01e55
Rodrigo Ferreira <rodrigoferreira001@hotmail.com>
2018-04-19 20:42:52 -0300

    test files created

--- darwin_batch_moreno.sh
+++ darwin_batch_moreno.sh
@@ -1,50 +1,162 @@
 #!/usr/bin/env bash
 
-#G 50 P 200 M 1
-for((number = 0; number < 5; number++)){
-	python gasir.py moreno_health/G50P200/moreno_health_G50_P200_M1.txt 200 127 --mutation 0.01 --generations 50
+# G 50 P 50M 1
+for((number = 0; number < 100; number++)){
+	 python gasir.py G50P50/moreno_health_G50_P50_M1.txt 50 127 --mutation 0.01 --generations 50
 }
 
-#G 50 P 200 M 5
-for((number = 0; number < 5; number++)){
-	python gasir.py moreno_health/G50P200/moreno_health_G50_P200_M5.txt 200 127 --mutation 0.05 --generations 50
+
+# G 50 P 50M 5
+for((number = 0; number < 100; number++)){
+	 python gasir.py G50P50/moreno_health_G50_P50_M5.txt 50 127 --mutation 0.05 --generations 50
 }
 
-#G 50 P 200 M 10
-for((number = 0; number < 5; number++)){
-	python gasir.py moreno_health/G50P200/moreno_health_G50_P200_M10.txt 200 127 --mutation 0.1 --generations 50
+
+# G 50 P 50M 10
+for((number = 0; number < 100; number++)){
+	 python gasir.py G50P50/moreno_health_G50_P50_M10.txt 50 127 --mutation 0.1 --generations 50
 }
 
-########################
 
-#G 100 P 100 M 1
-for((number = 0; number < 5; number++)){
-	python gasir.py moreno_health/G100P100/moreno_health_G100_P100_M1.txt 100 127 --mutation 0.01 --generations 100
+# G 100 P 50M 1
+for((number = 0; number < 100; number++)){
+	 python gasir.py G100P50/moreno_health_G100_P50_M1.txt 50 127 --mutation 0.01 --generations 100
 }
 
-#G 100 P 100 M 5
-for((number = 0; number < 5; number++)){
-	python gasir.py moreno_health/G100P100/moreno_health_G100_P100_M5.txt 100 127 --mutation 0.05 --generations 100
+
+# G 100 P 50M 5
+for((number = 0; number < 100; number++)){
+	 python gasir.py G100P50/moreno_health_G100_P50_M5.txt 50 127 --mutation 0.05 --generations 100
 }
 
-#G 100 P 100 M 10
-for((number = 0; number < 5; number++)){
-	python gasir.py moreno_health/G100P100/moreno_health_G100_P100_M10.txt 100 127 --mutation 0.1 --generations 100
+
+# G 100 P 50M 10
+for((number = 0; number < 100; number++)){
+	 python gasir.py G100P50/moreno_health_G100_P50_M10.txt 50 127 --mutation 0.1 --generations 100
 }
 
-########################
 
-#G 200 P 50 M 1
-for((number = 0; number < 5; number++)){
-	python gasir.py moreno_health/G200P50/moreno_health_G200_P50_M1.txt 50 127 --mutation 0.01 --generations 200
+# G 200 P 50M 1
+for((number = 0; number < 100; number++)){
+	 python gasir.py G200P50/moreno_health_G200_P50_M1.txt 50 127 --mutation 0.01 --generations 200
 }
 
-#G 200 P 50 M 5
-for((number = 0; number < 5; number++)){
-	python gasir.py moreno_health/G200P50/moreno_health_G200_P50_M5.txt 50 127 --mutation 0.5 --generations 200
+
+# G 200 P 50M 5
+for((number = 0; number < 100; number++)){
+	 python gasir.py G200P50/moreno_health_G200_P50_M5.txt 50 127 --mutation 0.05 --generations 200
 }
 
-#G 200 P 50 M 10
-for((number = 0; number < 5; number++)){
-	python gasir.py moreno_health/G200P50/moreno_health_G200_P50_M10.txt 50 127 --mutation 0.1 --generations 200
+
+# G 200 P 50M 10
+for((number = 0; number < 100; number++)){
+	 python gasir.py G200P50/moreno_health_G200_P50_M10.txt 50 127 --mutation 0.1 --generations 200
+}
+
+
+# G 50 P 100M 1
+for((number = 0; number < 100; number++)){
+	 python gasir.py G50P100/moreno_health_G50_P100_M1.txt 100 127 --mutation 0.01 --generations 50
+}
+
+
+# G 50 P 100M 5
+for((number = 0; number < 100; number++)){
+	 python gasir.py G50P100/moreno_health_G50_P100_M5.txt 100 127 --mutation 0.05 --generations 50
+}
+
+
+# G 50 P 100M 10
+for((number = 0; number < 100; number++)){
+	 python gasir.py G50P100/moreno_health_G50_P100_M10.txt 100 127 --mutation 0.1 --generations 50
+}
+
+
+# G 100 P 100M 1
+for((number = 0; number < 100; number++)){
+	 python gasir.py G100P100/moreno_health_G100_P100_M1.txt 100 127 --mutation 0.01 --generations 100
+}
+
+
+# G 100 P 100M 5
+for((number = 0; number < 100; number++)){
+	 python gasir.py G100P100/moreno_health_G100_P100_M5.txt 100 127 --mutation 0.05 --generations 100
+}
+
+
+# G 100 P 100M 10
+for((number = 0; number < 100; number++)){
+	 python gasir.py G100P100/moreno_health_G100_P100_M10.txt 100 127 --mutation 0.1 --generations 100
+}
+
+
+# G 200 P 100M 1
+for((number = 0; number < 100; number++)){
+	 python gasir.py G200P100/moreno_health_G200_P100_M1.txt 100 127 --mutation 0.01 --generations 200
+}
+
+
+# G 200 P 100M 5
+for((number = 0; number < 100; number++)){
+	 python gasir.py G200P100/moreno_health_G200_P100_M5.txt 100 127 --mutation 0.05 --generations 200
+}
+
+
+# G 200 P 100M 10
+for((number = 0; number < 100; number++)){
+	 python gasir.py G200P100/moreno_health_G200_P100_M10.txt 100 127 --mutation 0.1 --generations 200
+}
+
+
+# G 50 P 200M 1
+for((number = 0; number < 100; number++)){
+	 python gasir.py G50P200/moreno_health_G50_P200_M1.txt 200 127 --mutation 0.01 --generations 50
+}
+
+
+# G 50 P 200M 5
+for((number = 0; number < 100; number++)){
+	 python gasir.py G50P200/moreno_health_G50_P200_M5.txt 200 127 --mutation 0.05 --generations 50
+}
+
+
+# G 50 P 200M 10
+for((number = 0; number < 100; number++)){
+	 python gasir.py G50P200/moreno_health_G50_P200_M10.txt 200 127 --mutation 0.1 --generations 50
+}
+
+
+# G 100 P 200M 1
+for((number = 0; number < 100; number++)){
+	 python gasir.py G100P200/moreno_health_G100_P200_M1.txt 200 127 --mutation 0.01 --generations 100
+}
+
+
+# G 100 P 200M 5
+for((number = 0; number < 100; number++)){
+	 python gasir.py G100P200/moreno_health_G100_P200_M5.txt 200 127 --mutation 0.05 --generations 100
+}
+
+
+# G 100 P 200M 10
+for((number = 0; number < 100; number++)){
+	 python gasir.py G100P200/moreno_health_G100_P200_M10.txt 200 127 --mutation 0.1 --generations 100
+}
+
+
+# G 200 P 200M 1
+for((number = 0; number < 100; number++)){
+	 python gasir.py G200P200/moreno_health_G200_P200_M1.txt 200 127 --mutation 0.01 --generations 200
+}
+
+
+# G 200 P 200M 5
+for((number = 0; number < 100; number++)){
+	 python gasir.py G200P200/moreno_health_G200_P200_M5.txt 200 127 --mutation 0.05 --generations 200
+}
+
+
+# G 200 P 200M 10
+for((number = 0; number < 100; number++)){
+	 python gasir.py G200P200/moreno_health_G200_P200_M10.txt 200 127 --mutation 0.1 --generations 200
 }
---
+++ moreno_health/files_gen.py
@@ -0,0 +1,24 @@
+import os
+
+graph = open("moreno_health.txt", "r")
+ef = graph.read()
+
+pop = ["50","100","200"]
+gen = ["50", "100", "200"]
+mut = ["1","5","10"]
+
+for p in pop:
+	for g in gen:
+		for m in mut:
+			if not os.path.exists(str("G" + g + "P" + p)):
+				os.makedirs(str("G" + g + "P" + p))
+
+			filename = "G" + g + "P" + p + "/moreno_health_" + "G" + g + "_P" + p + "_M" + m + ".txt"
+			of = open(filename, "w")
+			of.write(ef)
+			of.close()
+
+			print "# G " + g + " P " + p + "M " + m
+			print "for((number = 0; number < 100; number++)){"
+			print "\t python gasir.py " + filename + " " + p + " 127 --mutation " +  str(int(m) / 100.0) + " --generations " + g
+			print "}\n\n"
