be24a48b3990eaadbc39eed90a5beaba044b2030
Rodrigo <Rodrigo Ferreira>
2018-06-04 16:40:24 -0300

    vln

--- bash.sh
+++ bash.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
-# G 50 P 400M 5
-for((number = 0; number < 100; number++)){
-         python gasir.py moreno_highschool/G50P400/moreno_highschool_G50_P400_M5.txt 400 8 --mutation 0.05 --generations 50 --infecteds 21
+# cagrqc
+for((number = 0; number < 10; number++)){
+         python gasir.py redes/cagrqc/cagrqc.txt 200 262 --mutation 0.05 --generations 50
+}
+
+# email_enron
+for((number = 0; number < 10; number++)){
+         python gasir.py redes/email_enron/email_enron.txt 200 1834 --mutation 0.05 --generations 50
+}
+
+# hep
+for((number = 0; number < 10; number++)){
+         python gasir.py redes/hep/hep.txt  200 761 --mutation 0.05 --generations 50
+}
+
+# phy
+for((number = 0; number < 10; number++)){
+         python gasir.py redes/phy/phy.txt  200 1857 --mutation 0.05 --generations 50
+}
+
+# Slashdot0811
+for((number = 0; number < 10; number++)){
+         python gasir.py redes/Slashdot0811/Slashdot0811.txt  200 3868 --mutation 0.05 --generations 50
 }
 
 
-# G 50 P 400M 10
-for((number = 0; number < 100; number++)){
-         python gasir.py moreno_highschool/G50P400/moreno_highschool_G50_P400_M10.txt 400 8 --mutation 0.1 --generations 50 --infecteds 21
-}
-
-
-# G 200 P 400M 5
-for((number = 0; number < 100; number++)){
-         python gasir.py moreno_highschool/G200P400/moreno_highschool_G200_P400_M5.txt 400 8 --mutation 0.05 --generations 200 --infecteds 21
-}
-
-
-# G 200 P 400M 10
-for((number = 0; number < 100; number++)){
-         python gasir.py moreno_highschool/G200P400/moreno_highschool_G200_P400_M10.txt 400 8 --mutation 0.1 --generations 200 --infecteds 21
-}
--- graph_adjust.py
+++ graph_adjust.py
@@ -5,6 +5,6 @@ gf = open(sys.argv[1], "r")
 fo = open(str(sys.argv[1].split(".")[0] + "_ad.txt"), "w")
 
 for line in gf.readlines():
-	values = line.split(" ")
-	fo.write( str(int(values[0]) - 1) + " " + str(int(values[1]) - 1) + "\n")
-	print str(int(values[0]) - 1) + " " + str(int(values[1]) - 1)
+	values = line.split("	")
+	fo.write( str(int(values[0])) + "	" + str(int(values[1])) + "\n")
+	print str(int(values[0]) - 1) + "	" + str(int(values[1]) - 1)
